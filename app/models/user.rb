eb1297f21f9835ce4e130242215c4abedb3dc111
Drue Wilding <drue@wilding.dk>
2026-06-28 10:39:06 +0200

    feat: Give users an inbox

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+    :recoverable, :rememberable, :validatable
+
+  has_many :lists, dependent: :destroy
+
+  def inbox
+    lists.find_by(is_default: true)
+  end
 end
