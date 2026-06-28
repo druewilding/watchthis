868c584dfd3e20594c1dbb4f826b6db0e590c562
Drue Wilding <drue@wilding.dk>
2026-06-28 10:42:44 +0200

    fix: Unrequire authentication for the API status endpoint

--- app/controllers/api/v1/status_controller.rb
+++ app/controllers/api/v1/status_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class StatusController < ApplicationController
+      skip_before_action :authenticate_user!
+
       def show
         render json: {status: "OK", message: "API is running"}
       end
