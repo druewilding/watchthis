b4c13c63e107a17b980b22c13bd6c0730ba86f4a
Drue Wilding <drue@wilding.dk>
2026-06-28 21:12:39 +0200

    feat: Improve the signup form to include name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name])
+  end
 end
