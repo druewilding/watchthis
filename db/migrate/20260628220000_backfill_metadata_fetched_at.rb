5cc71ef02fa8b862b5c02893eb30af54785b1b91
Drue Wilding <drue@wilding.dk>
2026-06-28 17:09:34 +0200

    style: Run the standards

--- db/migrate/20260628220000_backfill_metadata_fetched_at.rb
+++ db/migrate/20260628220000_backfill_metadata_fetched_at.rb
@@ -3,5 +3,6 @@ class BackfillMetadataFetchedAt < ActiveRecord::Migration[8.1]
     execute "UPDATE media SET metadata_fetched_at = created_at WHERE metadata_fetched_at IS NULL"
   end
 
-  def down; end
+  def down
+  end
 end
