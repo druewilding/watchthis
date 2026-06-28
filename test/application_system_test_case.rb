2fa95a9120c8a48b3dbcbd302eff2c9b6b7e96e2
Drue Wilding <drue@wilding.dk>
2026-06-28 21:35:58 +0200

    test: Disable parallelization on system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,9 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+
+  # System tests run a real browser against the Rails server, so they cannot
+  # share a database transaction with the test process. Disable parallelisation
+  # here so all system tests run in a single process and see the same DB state.
+  parallelize(workers: 1)
 end
